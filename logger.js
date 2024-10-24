09048b05e68f7783f72028974c82416ee9cdd1f6
ibratimoth <ibratimoth@gmail.com>
2024-10-24 12:56:20 +0300

    last

--- logger.js
+++ logger.js
@@ -1,24 +1,29 @@
-// logger.js
-const { createLogger, format, transports } = require('winston');
+const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, printf, colorize } = format;
 
 // Custom log format
 const logFormat = printf(({ level, message, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`;
+  return `${timestamp} ${level}: ${message}`;
 });
 
 // Create logger
 const logger = createLogger({
-    format: combine(
-        timestamp(),
-        colorize(), // colorize console output
-        logFormat
-    ),
-    transports: [
-        new transports.Console(),  // Log to console
-        new transports.File({ filename: './logs/app.log', level: 'info' }),  // Log all info level and above
-        new transports.File({ filename: './logs/error.log', level: 'error' }) // Log only error level
-    ],
+  level: "info", // Set the global log level to 'info'
+  format: combine(
+    timestamp(),
+    colorize(), // Colorize console output
+    logFormat
+  ),
+  transports: [
+    new transports.Console(), // Log to console
+    new transports.File({ filename: "./logs/app.log", level: "info" }), // Log all info level and above
+    new transports.File({ filename: "./logs/error.log", level: "error" }), // Log only error level
+  ],
+});
+
+// Listen for logging errors
+logger.on("error", (err) => {
+  console.error("Logging error:", err);
 });
 
 module.exports = logger;
